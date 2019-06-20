c90daff74fe1d657a0ecab941012c2bdca5af343
AAmanzi <alnick.amanzi@gmail.com>
2019-06-20 12:47:12 +0200

    Added Cashier and CashRegister controllers

---
+++ CashRegister.Web/Controllers/CashRegisterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CashRegister.Domain.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashRegister.Web.Controllers
+{
+    [Route("api/cash-registers")]
+    [ApiController]
+    public class CashRegisterController : ControllerBase
+    {
+        private readonly ICashRegisterRepository _cashRegisterRepository;
+
+        public CashRegisterController(ICashRegisterRepository cashRegisterRepository)
+        {
+            _cashRegisterRepository = cashRegisterRepository;
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAllCashRegisters()
+        {
+            return Ok(_cashRegisterRepository.GetAllCashRegisters());
+        }
+
+        [HttpGet("get-by-id")]
+        public IActionResult GetCashRegisterById(int id)
+        {
+            var cashRegister = _cashRegisterRepository.GetCashRegisterById(id);
+
+            if (cashRegister == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cashRegister);
+        }
+    }
+}
---
+++ CashRegister.Web/Controllers/CashierController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CashRegister.Domain.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashRegister.Web.Controllers
+{
+    [Route("api/cashiers")]
+    [ApiController]
+    public class CashierController : ControllerBase
+    {
+        private readonly ICashierRepository _cashierRepository;
+
+        public CashierController(ICashierRepository cashierRepository)
+        {
+            _cashierRepository = cashierRepository;
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAllCashiers()
+        {
+            return Ok(_cashierRepository.GetAllCashiers());
+        }
+
+        [HttpGet("get-by-id")]
+        public IActionResult GetCashierById(int id)
+        {
+            var cashier = _cashierRepository.GetCashierById(id);
+
+            if (cashier == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cashier);
+        }
+    }
+}
