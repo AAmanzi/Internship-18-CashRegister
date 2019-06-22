0c78cd0c897aca43b38ad0027725251dda47e850
AAmanzi <alnick.amanzi@gmail.com>
2019-06-22 19:03:35 +0200

    Added check for nulls in StringHelper

--- CashRegister.Domain/Helpers/StringHelper.cs
+++ CashRegister.Domain/Helpers/StringHelper.cs
@@ -8,6 +8,11 @@ namespace CashRegister.Domain.Helpers
     {
         public static bool IsDigitsOnly(string toTest)
         {
+            if (toTest == null)
+            {
+                return false;
+            }
+
             foreach (var character in toTest)
             {
                 if (character < '0' || character > '9')
